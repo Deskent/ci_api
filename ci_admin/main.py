7876c9586dd3ad788d8c6a6b82c548f8f9ed0077
Deskent <battenetciz@gmail.com>
2022-11-07 16:46:58 +0300

    v 1.1.6
    add admin

--- ci_admin/main.py
+++ ci_admin/main.py
@@ -2,13 +2,31 @@ from pathlib import Path
 
 import uvicorn
 from fastapi import FastAPI
+from sqladmin import Admin, ModelView
+from database.db import engine
+
+from models import User, Video
+
 
 DOCS_URL = "/ci_admin"
+BASE_DIR = Path(__file__).parent
+
 app = FastAPI(docs_url=DOCS_URL, redoc_url=DOCS_URL)
 
 
-BASE_DIR = Path(__file__).parent
+admin = Admin(app, engine)
 
+
+class UserAdmin(ModelView, model=User):
+    column_list = [User.id, User.username]
+
+
+class VideoView(ModelView, model=Video):
+    column_list = [Video.id, Video.path]
+
+
+admin.add_view(UserAdmin)
+admin.add_view(VideoView)
 
 if __name__ == '__main__':
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
---
+++ ci_admin/models.py
@@ -0,0 +1,127 @@
+from datetime import datetime, time
+from typing import Optional, List
+
+from pydantic import EmailStr, validator
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlmodel import SQLModel, Field, Relationship
+
+
+class AlarmBase(SQLModel):
+    alarm_time: time
+    text: Optional[str] = Field(nullable=True, default='')
+
+
+class AlarmCreate(AlarmBase):
+    user_id: Optional[int] = Field(default=None, foreign_key="users.id")
+
+
+class AlarmUpdate(AlarmCreate):
+    pass
+
+
+class Alarm(AlarmCreate, table=True):
+    __tablename__ = 'alarms'
+
+    id: int = Field(default=None, primary_key=True, index=True)
+    user: Optional['User'] = Relationship(back_populates="alarms")
+
+
+class NotificationBase(SQLModel):
+    notification_time: time
+    text: Optional[str] = Field(nullable=True, default='')
+
+
+class NotificationCreate(NotificationBase):
+    user_id: Optional[int] = Field(default=None, foreign_key="users.id")
+
+
+class NotificationUpdate(NotificationCreate):
+    pass
+
+
+class Notification(NotificationCreate, table=True):
+    __tablename__ = 'notifications'
+
+    id: int = Field(default=None, primary_key=True, index=True)
+    users: List['User'] = Relationship(back_populates="notifications")
+
+
+class VideoBase(SQLModel):
+    path: str
+    name: Optional[str] = Field(nullable=True, default='')
+    description: Optional[str] = Field(nullable=True, default='')
+    previous_id: Optional[int]
+    next_id: int
+
+
+class VideoCreate(VideoBase):
+    pass
+
+
+class Video(VideoBase, table=True):
+    __tablename__ = 'videos'
+
+    id: int = Field(default=None, primary_key=True, index=True)
+
+
+class UserCreate(SQLModel):
+    username: str = Field(nullable=False)
+    email: EmailStr = Field(unique=True, index=True)
+    password: str = Field(nullable=False, max_length=256, min_length=6)
+
+
+class UserInput(UserCreate):
+    password2: str
+
+    @validator('password2')
+    def password_match(cls, v, values, **kwargs):
+        if 'password' in values and v != values['password']:
+            raise ValueError('passwords don\'t match')
+        return v
+
+
+class UserLogin(SQLModel):
+    email: EmailStr
+    password: str
+
+
+class UserFullData(SQLModel):
+    is_admin: Optional[bool] = Field(default=False)
+    is_active: Optional[bool] = Field(default=False)
+    current_video: Optional[int] = Field(nullable=True, default=None, foreign_key='videos.id')
+    expired_at: Optional[datetime] = Field(default=None)
+
+
+class UserUpdate(UserFullData):
+    username: Optional[str]
+    email: Optional[EmailStr]
+    password: Optional[str]
+
+
+class UserOutput(UserFullData):
+    username: Optional[str]
+    email: Optional[EmailStr]
+
+
+class User(UserCreate, UserFullData, table=True):
+    __tablename__ = 'users'
+
+    id: int = Field(default=None, primary_key=True, index=True)
+    created_at: datetime = Field(default=datetime.now(tz=None))
+    alarms: List[Alarm] = Relationship(back_populates="user")
+    notifications: List[Notification] = Relationship(back_populates="users")
+
+    @classmethod
+    async def get_user_by_id(cls, session: AsyncSession, user_id: int) -> 'User':
+        query = select(cls).where(cls.id == user_id)
+        user = await session.execute(query)
+
+        return user.scalars().first()
+
+    @classmethod
+    async def get_user_by_email(cls, session: AsyncSession, email: EmailStr) -> 'User':
+        query = select(cls).where(cls.email == email)
+        user = await session.execute(query)
+
+        return user.scalars().first()
