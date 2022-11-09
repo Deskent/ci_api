0a314ed09e6db4a0e1baee6f68d585507c77f212
Deskent <battenetciz@gmail.com>
2022-11-09 17:26:00 +0300

    v 1.1.7
    add View for Complex

--- ci_api/admin/views.py
+++ ci_api/admin/views.py
@@ -8,23 +8,26 @@ class UserView(ModelView, model=User):
     name_plural = "Users"
     column_details_exclude_list = [User.password]
     column_list = [
-        User.id, User.username, User.email, User.current_video, User.current_complex, User.level,
-        User.progress, User.notifications, User.alarms, User.is_admin, User.is_active
+        User.id, User.username, User.email, User.current_video, User.current_complex,
+        User.level, User.progress, User.notifications, User.alarms, User.is_admin, User.is_active
     ]
     column_searchable_list = [User.username]
     column_sortable_list = [User.username]
 
 
 class VideoView(ModelView, model=Video):
-    column_list = [Video.id, Video.name, Video.description, Video.path]
+    column_list = [Video.id, Video.name, Video.description, Video.path, Video.complexes]
 
 
 class AlarmView(ModelView, model=Alarm):
-    column_list = [Alarm.id, Alarm.alarm_time, Alarm.text]
+    column_list = [Alarm.id, Alarm.alarm_time, Alarm.text, Alarm.users]
 
 
 class NotificationView(ModelView, model=Notification):
-    column_list = [Notification.id, Notification.notification_time, Notification.notification_time]
+    column_list = [
+        Notification.id, Notification.notification_time, Notification.notification_time,
+        Notification.users
+    ]
 
 
 class ComplexView(ModelView, model=Complex):
