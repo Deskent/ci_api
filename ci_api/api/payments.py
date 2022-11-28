0024de0d9135dc563e626d115ece8606dd91a262
Deskent <battenetciz@gmail.com>
2022-11-28 17:12:29 +0300

    v 1.5.19
    fix save file path

--- ci_api/api/payments.py
+++ ci_api/api/payments.py
@@ -3,7 +3,7 @@ import json
 from fastapi import APIRouter, Depends, status, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from config import logger
+from config import logger, settings
 from database.db import get_db_session
 
 router = APIRouter(prefix="/payments", tags=['Payments'])
@@ -18,5 +18,5 @@ async def payments(
         session: AsyncSession = Depends(get_db_session)
 ):
     logger.debug(f"payments data: {data}")
-    with open('payments.json', 'w', encoding='utf-8') as f:
+    with open(settings.LOGS_DIR / 'payments.json', 'w', encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
