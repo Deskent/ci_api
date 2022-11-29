8f7d1849fb5d7f35ef5a613e60bc4fcaa67bd990
Deskent <battenetciz@gmail.com>
2022-11-29 16:32:11 +0300

    v 1.5.21
    add video and complex viewed to DB
    add calculate remain videos duration
    add getting viewed videos to context
    add get_videos_duration method for Video model
    add row existing check for ViewedComplex and ViewedVideo

--- ci_api/services/complexes_and_videos.py
+++ ci_api/services/complexes_and_videos.py
@@ -1,24 +1,76 @@
 from sqlmodel.ext.asyncio.session import AsyncSession
 
 from config import LEVEL_UP_PERCENTS, logger
-from models.models import User, Complex, Video
+from models.models import User, Complex, Video, ViewedVideos, ViewedComplexes
 
 
-async def check_level_up(user: User, session: AsyncSession) -> User:
+async def check_level_up(session: AsyncSession, user: User) -> User:
     """Get current percent user progress in current complex"""
 
     current_complex: Complex = await session.get(Complex, user.current_complex)
-    videos: list[Video] = await Video.get_all_by_complex_id(session, user.current_complex)
-    if not videos:
+    videos: int = len(await Video.get_all_by_complex_id(session, current_complex.id))
+    if not current_complex.video_count:
         return user
-    percent: float = round(1 / len(videos), 1) * 100
+    percent: float = round(1 / videos, 1) * 100
     user.progress = user.progress + percent
     if user.progress >= LEVEL_UP_PERCENTS:
         if user.level < 10:
             user.level = user.level + 1
         user.progress = 0
+        await set_complex_viewed(session, user)
         user.current_complex = current_complex.next_complex_id
     await user.save(session)
     logger.debug(f"User with id {user.id} viewed video in complex {current_complex.id}")
 
     return user
+
+
+async def get_viewed_videos_ids(
+        session: AsyncSession,
+        user: User
+) -> tuple[int]:
+    viewed_videos: list[ViewedVideos] = await ViewedVideos.get_all_viewed_videos(session, user.id)
+
+    return tuple(elem.video_id for elem in viewed_videos)
+
+
+async def get_not_viewed_videos_ids(
+        videos: list[Video],
+        viewed_videos_ids: tuple[int]
+) -> tuple[int]:
+
+    return tuple(
+        video.id
+        for video in videos
+        if video.id not in viewed_videos_ids
+    )
+
+
+async def calculate_viewed_videos_duration(
+        session: AsyncSession,
+        not_viewed_ids: tuple[int]
+) -> int:
+    duration: int = await Video.get_videos_duration(session, not_viewed_ids)
+
+    return duration
+
+
+def calculate_videos_to_next_level(user: User, videos: list[Video]):
+    return int((LEVEL_UP_PERCENTS - user.progress) / (100 / len(videos)))
+
+
+async def set_video_viewed(
+        session: AsyncSession,
+        user: User,
+        video_id: int
+
+) -> ViewedVideos:
+    return await ViewedVideos.add_viewed(session, user.id, video_id)
+
+
+async def set_complex_viewed(
+        session: AsyncSession,
+        user: User
+
+) -> ViewedComplexes:
+    return await ViewedComplexes.add_viewed(session, user.id, user.current_complex)
--- ci_api/tests/test_database/test_complex_methods.py
+++ ci_api/tests/test_database/test_complex_methods.py
@@ -1,31 +1,32 @@
 # import pytest
-#
-from database.db import SQLModel, AsyncSession, engine, get_db_session
+# #
+# from database.db import SQLModel, AsyncSession, engine, get_db_session
 # from models.models import Complex, Video
-#
-#
-# # @pytest_asyncio.fixture
-# # def engine():
-# #     yield engine
-# #     # engine.sync_engine.dispose()
 # #
 # #
-# # @pytest_asyncio.fixture
-# # async def create(engine):
-# #     async with engine.begin() as conn:
-# #         await conn.run_sync(SQLModel.metadata.create_all)
-# #     yield
-# #     # async with engine.begin() as conn:
-# #     #     await conn.run_sync(SQLModel.metadata.drop_all)
-# #
-# #
-# # @pytest_asyncio.fixture
-# # async def session(engine, create):
-# #     async with AsyncSession(engine) as session:
-# #         yield session
+# # # @pytest_asyncio.fixture
+# # # def engine():
+# # #     yield engine
+# # #     # engine.sync_engine.dispose()
+# # #
+# # #
+# # # @pytest_asyncio.fixture
+# # # async def create(engine):
+# # #     async with engine.begin() as conn:
+# # #         await conn.run_sync(SQLModel.metadata.create_all)
+# # #     yield
+# # #     # async with engine.begin() as conn:
+# # #     #     await conn.run_sync(SQLModel.metadata.drop_all)
+# # #
+# # #
+# # @pytest.fixture
+# # def session():
+# #     return AsyncSession(engine)
+# #     # async with AsyncSession(engine) as session:
+# #     #     yield session
 #
 #
-# @pytest_asyncio.fixture
+# @pytest.fixture
 # def get_complex_data() -> dict:
 #     return {
 #                "description": "Описание комплекса 1",
@@ -36,7 +37,7 @@ from database.db import SQLModel, AsyncSession, engine, get_db_session
 #            }
 #
 #
-# @pytest_asyncio.fixture
+# @pytest.fixture
 # def get_video_data() -> dict:
 #     return {
 #         "description": "Описание тестового видео",
@@ -45,18 +46,19 @@ from database.db import SQLModel, AsyncSession, engine, get_db_session
 #         "complex_id": 1,
 #         "duration": 30
 #     }
-#
-#
-# @pytest_asyncio.fixture
-# async def session():
-#     return get_db_session
-#     # async for session in get_db_session():
-#     #     return session
-#
-#
-# # @pytest.mark.skip
+# #
+# #
+# # @pytest_asyncio.fixture
+# # async def session():
+# #     return get_db_session
+# #     # async for session in get_db_session():
+# #     #     return session
+# #
+# #
+# # # @pytest.mark.skip
 # # @pytest_asyncio.mark.anyio
-# async def test_add_complex(session, get_complex_data):
+# async def test_add_complex(get_complex_data):
+#     session = AsyncSession(engine)
 #     new_complex = await Complex.add_new(session=session, **get_complex_data)
 #     assert new_complex.id
 #     await new_complex.delete(session=session)
@@ -64,8 +66,8 @@ from database.db import SQLModel, AsyncSession, engine, get_db_session
 #
 # # @pytest.mark.skip
 # # @pytest_asyncio.mark.anyio
-# async def test_add_video(session, get_video_data):
-#     new_video = await Video.add_new(session=session, **get_video_data)
-#
-#     assert new_video.id
-#     await new_video.delete(session=session)
+# # async def test_add_video(session, get_video_data):
+# #     new_video = await Video.add_new(session=session, **get_video_data)
+# #
+# #     assert new_video.id
+# #     await new_video.delete(session=session)
