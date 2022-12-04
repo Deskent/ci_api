691c5feacf421b7555f416d91a3f6b5cfe47c415
Deskent <battenetciz@gmail.com>
2022-12-04 14:04:58 +0300

    v 1.7.2
    add web payment router

--- ci_api/web_service/router.py
+++ ci_api/web_service/router.py
@@ -2,9 +2,11 @@ from fastapi import APIRouter
 from web_service.endpoints.login import router as login_router
 from web_service.endpoints.profile import router as profile_router
 from web_service.endpoints.charging import router as charging_router
+from web_service.endpoints.payments import router as payments_router
 
 
 router = APIRouter()
 router.include_router(login_router)
 router.include_router(profile_router)
 router.include_router(charging_router)
+router.include_router(payments_router)
