4bde82cd5685c455d8582130fa1105f43eede3a1
Deskent <battenetciz@gmail.com>
2022-12-06 14:24:39 +0300

    v 1.7.12
    add level up in db

--- ci_api/web_service/endpoints/come_tomorrow.py
+++ ci_api/web_service/endpoints/come_tomorrow.py
@@ -2,7 +2,7 @@ from fastapi import Depends, APIRouter
 from starlette.responses import HTMLResponse
 
 from config import templates
-from models.models import User
+from models.models import User, ViewedComplex, Complex
 from web_service.utils.title_context_func import update_title
 from web_service.utils.titles_context import get_logged_user_context, get_user_from_context
 
@@ -16,6 +16,11 @@ async def come_tomorrow(
         user: User = Depends(get_user_from_context)
 ):
     await user.level_up()
+    current_complex: Complex = await Complex.get_by_id(user.current_complex)
+    await ViewedComplex.add_viewed(user.id, user.current_complex)
+    user.current_complex = await current_complex.next_complex_id()
+    await user.save()
+
     return templates.TemplateResponse(
         "come_tomorrow.html", context=update_title(context, "come_tomorrow.html")
     )
