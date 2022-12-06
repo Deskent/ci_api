3192781627f4add293586f1f3a540afe802cf659
Deskent <battenetciz@gmail.com>
2022-12-06 14:09:43 +0300

    v 1.7.12
    fix level  + 1 againt
    add level up in db

--- ci_api/web_service/endpoints/come_tomorrow.py
+++ ci_api/web_service/endpoints/come_tomorrow.py
@@ -2,8 +2,9 @@ from fastapi import Depends, APIRouter
 from starlette.responses import HTMLResponse
 
 from config import templates
+from models.models import User
 from web_service.utils.title_context_func import update_title
-from web_service.utils.titles_context import get_logged_user_context
+from web_service.utils.titles_context import get_logged_user_context, get_user_from_context
 
 
 router = APIRouter(tags=['web', 'tomorrow'])
@@ -12,7 +13,9 @@ router = APIRouter(tags=['web', 'tomorrow'])
 @router.get("/come_tomorrow", response_class=HTMLResponse)
 async def come_tomorrow(
         context: dict = Depends(get_logged_user_context),
+        user: User = Depends(get_user_from_context)
 ):
+    await user.level_up()
     return templates.TemplateResponse(
         "come_tomorrow.html", context=update_title(context, "come_tomorrow.html")
     )
