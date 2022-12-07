0d7c2936a9faf994b80ab825a8ad7cddb3d1732b
Juliya <juliyaone123@gmail.com>
2022-12-07 13:31:52 +0300

    Верстка без рабочего слайдера

--- static/js/app.js
+++ static/js/app.js
@@ -68,16 +68,7 @@ function makeBurgerMenu() {
    }
 }
 
-//Решили не редактировать имя пользователя
-// function editUserName() {
-//    const userNameLabel = document.querySelector('.user-profile__name_label')
-//    if (userNameLabel) {
-//       userNameLabel.addEventListener("click", () => {
-//          const userNameInput = userNameLabel.control;
-//          userNameInput.disabled = false;
-//       })
-//    }
-// }
+
 
 
 
@@ -268,26 +259,12 @@ document.addEventListener("keydown", function (e) {
 
 
 
-// смотрим сколько минут длится видео, создаем и вставляем html-елемент
 
-// function getVideoTime() {
-//    const modalVideos = document.querySelectorAll('.modal-video');
 
-//    modalVideos.forEach((video) => {
 
-//       let timeVideo = video.duration;
-//       console.log(timeVideo);
 
-//       const timeVideoElement = document.createElement("div");
-//       timeVideoElement.classList.add("modal-video__time");
-//       timeVideoElement.innerHTML = timeVideo + " мин";
 
-//       let modalVideoWrapper = video.closest(".modal-video__card-wrapper");
-//       modalVideoWrapper.append(timeVideoElement);
-//    })
-// }
 
-// getVideoTime();
 
 
 
@@ -316,9 +293,6 @@ function modalVideoControls() {
          let modalVideoDescription = modalVideoWrapperId.querySelector('.modal-video__description');
 
 
-
-
-
          if (modalVideoPause.classList.contains('hidden')) {
             modalVideoPlay.classList.add("hidden");
             if(videoId.classList.contains('viewed')) {
@@ -330,6 +304,7 @@ function modalVideoControls() {
             modalVideoTime.classList.add("hidden");
             modalVideoTitle.classList.add("hidden");
             modalVideoDescription.classList.add("hidden");
+
          } else {
             modalVideoPlay.classList.remove("hidden");
             videoId.pause();
@@ -354,94 +329,83 @@ function modalVideoEnded() {
 
    modalVideos.forEach((video, index, arr) => {
 
+      if(index == 0 ) {
+         video.classList.remove("lock");
+      }
+
       video.addEventListener("ended", async (evt) => {
 
-      // включаем следующеe видео и показываем модалку на следующий комлекс
-      const lengthArrVideo = modalVideos.length - 1;
+            const video_id = Number(evt.target.dataset.id);
+            let userPhone = document.querySelector('.header-user__phone');
+            const user_tel = userPhone.textContent;
+            const phone = user_tel.replace(/\s/g,'');
 
-      if(index < lengthArrVideo) {
-         const nextVideo = index + 2;
-         const modalNextVideos = document.querySelector('.modal-video[data-id="' + nextVideo + '"]');
-         console.log(modalNextVideos);
-         let modalVideoNextWrapperId = modalNextVideos.closest(".modal-video__card-wrapper");
+            const response = await fetch('http://127.0.0.1:8000/api/v1/videos/viewed', {
+               method: 'POST',
+               body: JSON.stringify({
+                  "phone": phone,
+                  "video_id": video_id
+               }),
+               headers: {
+               'Content-Type': 'application/json;charset=utf-8',
+               },
+            });
+            
+            let result = await response.json();
 
-         modalNextVideos.play();
-         let modalNextVideosPlay = modalVideoNextWrapperId.querySelector('.modal-video__play');
-         let modalNextVideosPause = modalVideoNextWrapperId.querySelector('.modal-video__pause');
-         modalNextVideosPlay.classList.add("hidden");
-         modalNextVideosPause.classList.remove("hidden");
-      }
+            const lengthArrVideo = modalVideos.length - 1;
 
-      const modalNewLevel = document.querySelector('.new-level-box');
+            //нашли следующее видео для воспроизведения
+            if(index < lengthArrVideo) {
+               const modalNextVideos = document.getElementById(result.next_video_id);
+               modalNextVideos.play();
+               let modalVideoNextWrapperId = modalNextVideos.closest(".modal-video__card-wrapper");
+               let modalNextVideosPlay = modalVideoNextWrapperId.querySelector('.modal-video__play');
+               let modalNextVideosPause = modalVideoNextWrapperId.querySelector('.modal-video__pause');
+               let modalVideoNextLock = modalVideoNextWrapperId.querySelector('.modal-video.lock');
+               let modalVideoViewed = modalVideoNextWrapperId.querySelector('.modal-video__viewed');
+               let modalVideoTime = modalVideoNextWrapperId.querySelector('.modal-video__time');
 
+               modalNextVideosPlay.classList.add("hidden");
+               modalNextVideosPause.classList.remove("hidden");
+               modalVideoNextLock.classList.remove("lock");
+               modalVideoViewed.classList.add("hidden");
+               modalVideoTime.classList.add("hidden");
+            }
 
-      if(index === lengthArrVideo) {
-         if(modalNewLevel.classList.contains('hidden')) {
-            modalNewLevel.classList.remove('hidden');
-         }
-      }
-
-
-
-
-
-
-      // поменять стили на "видео просмотрено"
-
-         let targetModalVideoEnded = evt.target.dataset.id;
-         console.log("Видео " + targetModalVideoEnded + " просмотрено!");
-
-         let videoIdEnded = document.getElementById(targetModalVideoEnded);
-         let modalVideoWrapperId = videoIdEnded.closest(".modal-video__card-wrapper");
-         let modalVideoPlay = modalVideoWrapperId.querySelector('.modal-video__play');
-         let modalVideoPause = modalVideoWrapperId.querySelector('.modal-video__pause');
-
-         let modalVideoViewedHidden = modalVideoWrapperId.querySelector('.modal-video__viewed');
-         modalVideoViewedHidden.classList.remove("hidden");
-
-         modalVideoViewedHidden.textContent = "Просмотрено"
-
-         let modalVideoTime = modalVideoWrapperId.querySelector('.modal-video__time');
-         let modalVideoTitle = modalVideoWrapperId.querySelector('.modal-video__title');
-         let modalVideoDescription = modalVideoWrapperId.querySelector('.modal-video__description');
-
-         modalVideoTime.classList.remove("hidden");
-         videoIdEnded.classList.add("viewed");
-         modalVideoPlay.classList.remove("hidden");
-         modalVideoPause.classList.add("hidden");
-         modalVideoTitle.classList.remove("hidden");
-         modalVideoDescription.classList.remove("hidden");
+            const modalNewLevel = document.querySelector('.new-level-box');
 
 
+            if(index === lengthArrVideo) {
+               if(modalNewLevel.classList.contains('hidden')) {
+                  modalNewLevel.classList.remove('hidden');
+               }
+            }
 
+            // поменяли стили на "видео просмотрено"
+            let targetModalVideoEnded = evt.target.dataset.id;
+            console.log("Видео " + targetModalVideoEnded + " просмотрено!");
 
-      //отправить fetch о том, что видео просмотрено
+            let videoIdEnded = document.getElementById(targetModalVideoEnded);
+            let modalVideoWrapperId = videoIdEnded.closest(".modal-video__card-wrapper");
+            
+            let modalVideoPlay = modalVideoWrapperId.querySelector('.modal-video__play');
+            let modalVideoPause = modalVideoWrapperId.querySelector('.modal-video__pause');
+            let modalVideoViewedHidden = modalVideoWrapperId.querySelector('.modal-video__viewed');
+            let modalVideoTitle = modalVideoWrapperId.querySelector('.modal-video__title');
+            let modalVideoDescription = modalVideoWrapperId.querySelector('.modal-video__description');
+            let modalVideoTime = modalVideoWrapperId.querySelector('.modal-video__time');
 
-         const video_id = Number(evt.target.dataset.id);
-         let userPhone = document.querySelector('.header-user__phone');
-         const user_tel = userPhone.textContent;
-         const phone = user_tel.replace(/\s/g,'');
-         // console.log( user_tel1);
-
-         const response = await fetch('http://127.0.0.1:8000/api/v1/videos/viewed', {
-            method: 'POST',
-            body: JSON.stringify({
-               "user_tel": phone,
-               "video_id": video_id
-            }),
-            headers: {
-            'accept': 'application/json',
-            'Content-Type': 'application/json;charset=utf-8',
-            'Access-Control-Allow-Origin': 'no-cors'
-            },
-         });
-         let result = await response.json();
-         console.log(result.json);
+            modalVideoTime.classList.remove("hidden");
+            modalVideoViewedHidden.classList.remove("hidden");
+            modalVideoViewedHidden.textContent = "Просмотрено"
+            videoIdEnded.classList.add("viewed");
+            modalVideoPlay.classList.remove("hidden");
+            modalVideoPause.classList.add("hidden");
+            modalVideoTitle.classList.remove("hidden");
+            modalVideoDescription.classList.remove("hidden");
 
          });
-
-
-
       })
 }
 
