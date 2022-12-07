19211d9890ab07251dd477b789f7a370663e104f
Deskent <battenetciz@gmail.com>
2022-12-07 16:47:31 +0300

    v 1.9
    add nginx container

--- ci_api/main.py
+++ ci_api/main.py
@@ -36,22 +36,22 @@ def get_application():
 
     app = FastAPI(docs_url=DOCS_URL, redoc_url=DOCS_URL, debug=settings.DEBUG)
 
-    origins = [
-        "http://localhost.tiangolo.com",
-        "https://localhost.tiangolo.com",
-        "http://localhost",
-        "http://localhost:8080",
-        "http://localhost:8000",
-        "http://127.0.0.1:8000",
-    ]
-
-    app.add_middleware(
-        CORSMiddleware,
-        allow_origins=origins,
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
-    )
+    # origins = [
+    #     "http://localhost.tiangolo.com",
+    #     "https://localhost.tiangolo.com",
+    #     "http://localhost",
+    #     "http://localhost:8080",
+    #     "http://localhost:8000",
+    #     "http://127.0.0.1:8000",
+    # ]
+    #
+    # app.add_middleware(
+    #     CORSMiddleware,
+    #     allow_origins=origins,
+    #     allow_credentials=True,
+    #     allow_methods=["*"],
+    #     allow_headers=["*"],
+    # )
 
     app.mount("/static", StaticFiles(directory=str(settings.STATIC_DIR)), name="static")
     app.mount("/templates", StaticFiles(directory=str(settings.TEMPLATES_DIR)), name="templates")
