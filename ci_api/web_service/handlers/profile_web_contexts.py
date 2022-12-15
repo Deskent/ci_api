bd6240560979a414c9b16f32fb25f465e2487159
Deskent <battenetciz@gmail.com>
2022-12-15 11:05:35 +0300

    [build:1.10.4] [version:1.0]
    split get_edit_profile_web_context

--- ci_api/web_service/handlers/profile_web_contexts.py
+++ ci_api/web_service/handlers/profile_web_contexts.py
@@ -9,57 +9,112 @@ from services.user import send_sms, check_user_phone_exists, check_email_exists
 from services.web_context_class import WebContext
 
 
+async def update_web_context_with_phone(
+        web_context: WebContext, user_data: UserEditProfile, user: User
+) -> tuple[WebContext, UserEditProfile, User]:
+    """Check user phone exists, send sms to new phone, update web_context and user
+    Not updating web_context template
+    """
+    logger.debug(f"Check phone exists {user_data}")
+    if await check_user_phone_exists(user_data.phone):
+        web_context.error = PhoneExistsError.detail
+        web_context.to_raise = PhoneExistsError
+
+        return web_context, user_data, user
+
+    result: dict = await send_sms(user_data.phone)
+    if sms_message := result.get('sms_message'):
+        logger.debug(f"Sms_message code: {sms_message}")
+        user.sms_message = sms_message
+        user.phone = user_data.phone
+
+    elif error := result.get('error'):
+        web_context.error = error
+        web_context.to_raise = SmsServiceError
+
+    return web_context, user_data, user
+
+
+async def update_web_context_with_email(
+        web_context: WebContext, user_data: UserEditProfile, user: User
+) -> tuple[WebContext, UserEditProfile, User]:
+    """Check user email exists, send code to new email,
+    update web_context and user
+    Not updating web_context template
+    """
+
+    logger.debug(f"Check email exists {user_data}")
+
+    if await check_email_exists(user_data.email):
+        web_context.error = EmailExistsError.detail
+        web_context.to_raise = EmailExistsError
+
+        return web_context, user_data, user
+    try:
+        code: str = await send_verification_mail(user_data.email)
+        logger.debug(f"Email code: {code}")
+        user.email_code = code
+        user.is_verified = False
+        user.expired_at = None
+        user.email = user_data.email
+    except EmailException:
+        web_context.error = "Неверный адрес почты"
+        web_context.to_raise = EmailError
+
+    return web_context, user_data, user
+
+
+async def _check_phone_changed(
+        web_context: WebContext, user_data: UserEditProfile, user: User
+) -> tuple[WebContext, UserEditProfile, User]:
+    """Check user phone changed"""
+
+    logger.debug(f"Check phone changed {user_data}")
+
+    if user_data.phone:
+        phone: str = slice_phone_to_format(user_data.phone)
+        if user.phone != phone:
+            web_context, user_data, user = await update_web_context_with_phone(web_context, user_data, user)
+            web_context.template = "edit_profile.html"
+
+    return web_context, user_data, user
+
+
+
+async def _check_email_changed(
+        web_context: WebContext, user_data: UserEditProfile, user: User
+) -> tuple[WebContext, UserEditProfile, User]:
+    """Check user email changed"""
+
+    logger.debug(f"Check email changed {user_data}")
+    if user_data.email and user.email != user_data.email:
+        web_context, user_data, user = await update_web_context_with_email(web_context, user_data, user)
+        web_context.template = "edit_profile.html"
+
+    return web_context, user_data, user
+
+
 async def get_edit_profile_web_context(
         context: dict,
         user_data: UserEditProfile,
         user: User
 ) -> WebContext:
+
     web_context = WebContext(context=context)
+    if user_data.username:
+        user.username = user_data.username
+    if user_data.last_name:
+        user.last_name = user_data.last_name
+    if user_data.third_name:
+        user.third_name = user_data.third_name
 
-    user.username = user_data.username
-    user.last_name = user_data.last_name
-    user.third_name = user_data.third_name
-    phone: str = slice_phone_to_format(user_data.phone)
-    if user.phone != phone:
+    web_context, user_data, user = await _check_phone_changed(web_context, user_data, user)
+    if web_context.error:
+        return web_context
 
-        if await check_user_phone_exists(user_data.phone):
-            web_context.error = PhoneExistsError.detail
-            web_context.template = "edit_profile.html"
-            web_context.to_raise = PhoneExistsError
-            return web_context
-
-        result: dict = await send_sms(user_data.phone)
-        if sms_message := result.get('sms_message'):
-            logger.debug(f"Sms_message code: {sms_message}")
-            user.sms_message = sms_message
-        elif error := result.get('error'):
-            web_context.error = error
-            web_context.template = "edit_profile.html"
-            web_context.to_raise = SmsServiceError
-
-            return web_context
-
-    user.phone = phone
-    if user.email != user_data.email:
-        if await check_email_exists(user_data.email):
-            web_context.error = EmailExistsError.detail
-            web_context.template = "edit_profile.html"
-            web_context.to_raise = EmailExistsError
-
-            return web_context
-        try:
-            code: str = await send_verification_mail(user_data.email)
-            logger.debug(f"Email code: {code}")
-            user.email_code = code
-            user.is_verified = False
-            user.expired_at = None
-            user.email = user_data.email
-        except EmailException:
-            web_context.error = "Неверный адрес почты"
-            web_context.template = "edit_profile.html"
-            web_context.to_raise = EmailError
-
-            return web_context
+    web_context, user_data, user = await _check_email_changed(web_context, user_data, user)
+    if web_context.error:
+        return web_context
 
     user = await user.save()
     web_context.context.update(user=user)
