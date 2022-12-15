d78b862fa03cadbaef336e9294629eab685bb06e
Deskent <battenetciz@gmail.com>
2022-12-15 09:53:36 +0300

    [build:1.10.3] [version:1.0]
    refactor with web_context

--- ci_api/web_service/handlers/enter_with_sms.py
+++ ci_api/web_service/handlers/enter_with_sms.py
@@ -5,14 +5,13 @@ from loguru import logger
 from starlette.requests import Request
 
 from config import settings
-from exc.exceptions import UserNotFoundError, SmsCodeNotValid, UserNotFoundErrorApi
+from exc.exceptions import SmsCodeNotValid, UserNotFoundErrorApi
 from models.models import User
 from schemas.user_schema import slice_phone_to_format
 from services.user import send_sms
 from services.web_context_class import WebContext
 from web_service.services.sms_class import sms_service, SMSException
-from web_service.utils.get_contexts import get_base_context, update_user_session_token, \
-    get_profile_page_context
+from web_service.utils.get_contexts import get_base_context, update_user_session_token
 
 
 async def entry_via_sms_or_call(
@@ -21,7 +20,7 @@ async def entry_via_sms_or_call(
         phone: str = Form(...),
 ) -> WebContext:
     web_context = WebContext(context=context)
-    phone = slice_phone_to_format(phone)
+    phone: str = slice_phone_to_format(phone)
     user: User = await User.get_by_phone(phone)
     if not user:
         web_context.error = "Пользователь с таким номером телефона не найден"
@@ -36,35 +35,35 @@ async def entry_via_sms_or_call(
     return web_context
 
 
-async def enter_via_phone_call(obj: WebContext, user: User) -> WebContext:
+async def enter_via_phone_call(web_context: WebContext, user: User) -> WebContext:
     try:
         code: str = await sms_service.send_call(phone=user.phone)
         if code:
             user.sms_call_code = str(code)
             await user.save()
-            obj.template = "forget3.html"
+            web_context.template = "forget3.html"
 
     except SMSException as err:
         logger.error(err)
-        obj.error = str(err)
-        obj.template = "entry_sms.html"
+        web_context.error = str(err)
+        web_context.template = "entry_sms.html"
 
-    return obj
+    return web_context
 
 
-async def enter_via_sms(obj: WebContext, user: User) -> WebContext:
+async def enter_via_sms(web_context: WebContext, user: User) -> WebContext:
 
     result: dict = await send_sms(user.phone)
     if sms_message := result.get('sms_message'):
         user.sms_message = sms_message
         await user.save()
     elif error := result.get('error'):
-        obj.error = error
+        web_context.error = error
 
-    obj.template = "forget2.html"
+    web_context.template = "forget2.html"
     if settings.STAGE == 'test':
-        obj.success = f'Тестовый режим: ваш код: {sms_message}'
-    return obj
+        web_context.success = f'Тестовый режим: ваш код: {sms_message}'
+    return web_context
 
 
 async def approve_sms_code(
@@ -75,7 +74,7 @@ async def approve_sms_code(
         phone: str = None,
 ) -> WebContext:
 
-    obj = WebContext(context=context)
+    web_context = WebContext(context=context)
 
     user: User | None = None
     if user_id:
@@ -85,14 +84,14 @@ async def approve_sms_code(
         user: User = await User.get_by_phone(phone)
 
     if not user:
-        obj.error = "Неверный номер телефона"
-        obj.template = "entry_sms.html"
-        obj.to_raise = UserNotFoundErrorApi
+        web_context.error = "Неверный номер телефона"
+        web_context.template = "entry_sms.html"
+        web_context.to_raise = UserNotFoundErrorApi
 
-        return obj
+        return web_context
 
-    obj.context.update(user=user)
-    obj.api_data.update(payload=user)
+    web_context.context.update(user=user)
+    web_context.api_data.update(payload=user)
     user_code: str = user.sms_message
     cleaner: Callable = user.clean_sms_code
     if request.url.path == "/forget3":
@@ -100,17 +99,16 @@ async def approve_sms_code(
         cleaner: Callable = user.clean_sms_call_code
 
     if code != user_code:
-        obj.error = f"Неверный код: {code}"
-        logger.debug(obj.error)
+        web_context.error = f"Неверный код: {code}"
+        logger.debug(web_context.error)
         url_path = f"{request.url.path}.html"
-        obj.template = url_path
-        obj.to_raise = SmsCodeNotValid
-        return obj
+        web_context.template = url_path
+        web_context.to_raise = SmsCodeNotValid
+        return web_context
 
     await cleaner()
     await user.set_verified()
     await update_user_session_token(request, user)
-    obj.context = await get_profile_page_context(obj.context)
-    obj.template = "profile.html"
+    web_context.template = "profile.html"
 
-    return obj
+    return web_context
