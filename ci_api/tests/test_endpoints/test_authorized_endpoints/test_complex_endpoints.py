222e05c11d3c2410bc7e0e0c4801b1c52663c70d
Deskent <battenetciz@gmail.com>
2022-12-17 10:01:42 +0300

    split tests
    [build:1.10.8]
    [version:1.0]

--- ci_api/tests/test_endpoints/test_authorized_endpoints/test_complex_endpoints.py
+++ ci_api/tests/test_endpoints/test_authorized_endpoints/test_complex_endpoints.py
@@ -4,14 +4,6 @@ from tests.test_endpoints.test_authorized_endpoints.base_test_class import BaseT
 class TestComplex(BaseTest):
 
     @pytest.mark.server
-    def test_all_for_complex_id(self):
-        response = self.session.get(self.base_url + "/videos/all_for/1", headers=self.headers)
-        assert response.status_code == 200
-        data: list = response.json()
-        assert data is not None
-        assert isinstance(data, list)
-
-    @pytest.mark.server
     def test_get_complexes_list(self):
         response = self.session.get(self.base_url + "/complex/list", headers=self.headers)
         assert response.status_code == 200
--- ci_api/tests/test_endpoints/test_authorized_endpoints/test_video_endpoints.py
+++ ci_api/tests/test_endpoints/test_authorized_endpoints/test_video_endpoints.py
@@ -3,6 +3,15 @@ from tests.test_endpoints.test_authorized_endpoints.base_test_class import BaseT
 
 
 class TestVideo(BaseTest):
+
+    @pytest.mark.server
+    def test_all_videos_for_complex_id(self):
+        response = self.session.get(self.base_url + "/videos/all_for/1", headers=self.headers)
+        assert response.status_code == 200
+        data: list = response.json()
+        assert data is not None
+        assert isinstance(data, list)
+
     @pytest.mark.skip('Need activate user')
     def test_get_video_by_id(self, event_loop):
         response = self.session.get(self.base_url + f"/complex/1/", headers=self.headers)
