eab45e46f240192bf0764492acb649fd3c04ac8f
Deskent <battenetciz@gmail.com>
2022-12-17 15:05:29 +0300

    refactoring to web_context
    [build:1.10.11]
    [version:1.0]

--- ci_api/api/web_api.py
+++ ci_api/api/web_api.py
@@ -4,8 +4,11 @@ from fastapi import APIRouter, status, Depends, Body
 
 from models.models import User, Complex, ViewedComplex
 from schemas.avatar import AvatarBase
+from schemas.complexes_videos import ComplexesListWithViewedAndNot
 from schemas.user_schema import UserOutput
+from services.complexes_web_context import get_complexes_list_web_context
 from services.videos_methods import get_viewed_complex_response
+from services.web_context_class import WebContext
 from web_service.utils.get_contexts import get_user_from_context
 from config import logger
 
@@ -24,36 +27,14 @@ async def complex_viewed_web(
     return await get_viewed_complex_response(user=user, complex_id=complex_id)
 
 
-@router.get("/complex/list", response_model=dict)
+@router.get("/complex/list", response_model=ComplexesListWithViewedAndNot)
 async def get_complexes_list_web(
         user: User = Depends(get_user_from_context)
 ):
     """Return viewed_complexes, today_complex, not_viewed_complexes, user"""
 
-    today_complex = None
-    if not await ViewedComplex.is_last_viewed_today(user.id):
-        today_complex: Complex | None = await Complex.get_by_id(user.id)
-    complexes: list[Complex] = await Complex.get_all()
-    user_viewed_complexes: list[int] = await ViewedComplex.get_all_viewed_complexes_ids(user.id)
-    viewed_complexes: list[Complex] = [
-        elem
-        for elem in complexes
-        if elem.id in user_viewed_complexes
-
-    ]
-    not_viewed_complexes: list[Complex] = [
-        elem
-        for elem in complexes
-        if elem.id not in user_viewed_complexes
-
-    ]
-
-    return dict(
-        user=UserOutput(**user.dict()),
-        viewed_complexes=viewed_complexes,
-        today_complex=today_complex,
-        not_viewed_complexes=not_viewed_complexes
-    )
+    web_context: WebContext = await get_complexes_list_web_context({}, user)
+    return web_context.api_render()
 
 
 @router.post("/set_avatar", status_code=204)
