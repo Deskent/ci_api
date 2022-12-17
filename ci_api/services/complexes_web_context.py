bc5a734c03e9de1872f5ca46f02a68c908ac7109
Deskent <battenetciz@gmail.com>
2022-12-17 16:44:41 +0300

    refactor api/complexes/list to web_context
    [build:1.10.13]
    [version:1.0]

--- ci_api/services/complexes_web_context.py
+++ ci_api/services/complexes_web_context.py
@@ -1,6 +1,7 @@
 from models.models import User, Complex, ViewedComplex
 from schemas.complexes_videos import ComplexesListWithViewedAndNot
 from schemas.user_schema import UserOutput
+from services.models_cache.base_cache import AllCache
 from services.web_context_class import WebContext
 
 
@@ -33,18 +34,17 @@ async def get_complexes_list_web_context(
     web_context = WebContext(context)
     web_context.context.update(user=user)
 
-    today_complex = None
+    today_complex = {}
     if not await ViewedComplex.is_last_viewed_today(user.id):
-        today_complex: Complex | None = await Complex.get_by_id(user.id)
+        today_complex: Complex = await AllCache.get_by_id(Complex, user.current_complex)
 
-    complexes: list[Complex] = await Complex.get_all()
+    complexes: list[Complex] = await AllCache.get_all(Complex)
     user_viewed_complexes: list[int] = await ViewedComplex.get_all_viewed_complexes_ids(user.id)
     viewed_complexes: list[Complex] = _get_viewed_complexes(complexes, user_viewed_complexes)
     not_viewed_complexes: list[Complex] = _get_not_viewed_complexes(complexes, user_viewed_complexes)
 
     result = ComplexesListWithViewedAndNot(
         user=UserOutput(**user.dict()),
-        complexes=complexes,
         viewed_complexes=viewed_complexes,
         today_complex=today_complex,
         not_viewed_complexes=not_viewed_complexes
