3f589e5a05f2773775cb499c90a1e61582a327a8
Deskent <battenetciz@gmail.com>
2022-12-19 00:34:20 +0300

    fix random_symbols if file exists
    [build:1.10.17]
    [version:1.0]

--- ci_api/api/web_api_utils.py
+++ ci_api/api/web_api_utils.py
@@ -1,3 +1,5 @@
+import datetime
+
 from fastapi import UploadFile
 from loguru import logger
 
@@ -16,7 +18,7 @@ async def set_avatar_from_file_web_context(
     web_context = WebContext(context)
     file_path = settings.MEDIA_DIR / 'avatars' / file.filename
     if file_path.exists():
-        random_symbols: str = services.utils.generate_four_random_digits_string()
+        random_symbols = int(datetime.datetime.now().timestamp())
         file_path = settings.MEDIA_DIR / 'avatars' / f"{random_symbols}_{file.filename}"
 
     save_uploaded_file(file_path, file)
