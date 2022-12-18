a2bbf77675762fdd6371ad19b70c62f78aefbc4b
Deskent <battenetciz@gmail.com>
2022-12-18 19:32:55 +0300

    add test_redis_save_rates
    [build:1.10.15]
    [version:1.0]

--- ci_api/tests/test_redis/test_redis_methods.py
+++ ci_api/tests/test_redis/test_redis_methods.py
@@ -1,16 +1,33 @@
 import pytest
 
 from config import REDIS_CLIENT
+from models.models import Rate
 from services.models_cache.redis_interface import RedisDB
 
 
-@pytest.mark.server
-async def test_redis_health_check():
-    result = await RedisDB(key='test', client=REDIS_CLIENT).health_check()
-    assert result['data'] == ['test']
-    assert await RedisDB(key='test').delete_key() is None
+@pytest.fixture
+def redis():
+    return RedisDB(client=REDIS_CLIENT)
+
 
 @pytest.mark.server
-async def test_redis_value_error():
+async def test_redis_health_check(redis):
+    redis.key = 'test'
+    result = await redis.health_check()
+    assert result['data'] == ['test']
+    assert await redis.delete_key() is None
+
+@pytest.mark.server
+async def test_redis_value_error(redis):
     with pytest.raises(ValueError):
-        await RedisDB(client=REDIS_CLIENT).health_check()
+        await redis.health_check()
+
+
+@pytest.mark.server
+async def test_redis_save_rates():
+    rates = await Rate.get_all()
+    redis = RedisDB(client=REDIS_CLIENT, model=Rate)
+    await redis.save(data=rates)
+    data: dict[str, list | dict] = await redis.load()
+    assert data['data'] == rates
+    assert data['data'][0].id == rates[0].id
