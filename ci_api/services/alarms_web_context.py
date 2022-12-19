c736d15e8d163ffa1ee3c6347992f2a98ea5ce1c
Deskent <battenetciz@gmail.com>
2022-12-19 16:21:39 +0300

    refactor Alarms to CRUD
    [build:1.11.3]
    [version:1.0]

--- ci_api/services/alarms_web_context.py
+++ ci_api/services/alarms_web_context.py
@@ -1,15 +1,17 @@
 from fastapi import status, HTTPException
 
+from exc.exceptions import AlarmNotFound
 from models.models import Alarm, User
 from schemas.alarms import AlarmUpdate
+from services.models_cache.crud import CRUD
 from services.web_context_class import WebContext
 from services.weekdays import WeekDay
 
 
 async def get_alarm_or_raise(alarm_id: int, user: User):
-    alarm: Alarm = await user.get_alarm_by_id(alarm_id)
+    alarm: Alarm = await CRUD.user.get_alarm_by_alarm_id(user, alarm_id)
     if not alarm:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Alarm not found")
+        raise AlarmNotFound
     return alarm
 
 
@@ -29,9 +31,8 @@ async def get_update_alarm_web_context(
             if getattr(alarm, key) != value:
                 setattr(alarm, key, value)
 
-    await alarm.save()
-    alarm.weekdays = WeekDay(alarm.weekdays).as_list
-    alarm.alarm_time = alarm.alarm_time.strftime("%H:%M")
+    alarm: Alarm = await CRUD.alarm.save(alarm)
+    alarm: Alarm = await CRUD.alarm.for_response(alarm)
     web_context.api_data.update(payload=alarm)
 
     return web_context
