5760f10d521d529d991867e9337e3ef84e02f155
Deskent <battenetciz@gmail.com>
2022-12-20 21:34:23 +0300

    fix to SQLModel
    [build:1.11.15]
    [version:1.0]

--- ci_api/models/models.py
+++ ci_api/models/models.py
@@ -2,14 +2,11 @@ from datetime import datetime, time
 from typing import Optional, List
 
 from pydantic import EmailStr
-from sqlalchemy import Column, TIMESTAMP, desc
-from sqlmodel import Field, Relationship, select
-
-from models.methods import BaseSQLModel, UserModel, get_all, get_first, AdminModel
-from services.utils import get_current_datetime
+from sqlalchemy import Column, TIMESTAMP
+from sqlmodel import Field, Relationship, SQLModel
 
 
-class Complex(BaseSQLModel, table=True):
+class Complex(SQLModel, table=True):
     __tablename__ = 'complexes'
 
     id: int = Field(default=None, primary_key=True, index=True)
@@ -29,7 +26,7 @@ class Complex(BaseSQLModel, table=True):
         return f"№ {self.number}: {self.description}"
 
 
-class Video(BaseSQLModel, table=True):
+class Video(SQLModel, table=True):
     __tablename__ = 'videos'
 
     id: int = Field(default=None, primary_key=True, index=True)
@@ -50,70 +47,15 @@ class Video(BaseSQLModel, table=True):
     def __str__(self):
         return f"{self.name}"
 
-    async def next_video_id(self) -> int:
-        query = select(Video).where(Video.number == self.number + 1)
-        next_video: Video = await get_first(query)
 
-        return 1 if not next_video else next_video.id
-
-    @classmethod
-    async def get_ordered_list(cls, complex_id: int):
-        query = select(cls).where(cls.complex_id == complex_id).order_by(cls.number)
-        return await get_all(query)
-
-    @classmethod
-    async def get_all_by_complex_id(cls, complex_id: int):
-        return await cls.get_ordered_list(complex_id)
-
-    @classmethod
-    async def add_new(
-            cls: 'Video',
-            file_name: str,
-            complex_id: int,
-            duration: int,
-            number: int,
-            name: str = '',
-            description: str = '',
-    ) -> 'Video':
-        new_video = Video(
-            name=name, description=description, complex_id=complex_id,
-            duration=duration, file_name=file_name, number=number
-        )
-        await new_video.save()
-        current_complex = await Complex.get_by_id(complex_id)
-        current_complex.duration += duration
-        current_complex.video_count += 1
-        await current_complex.save()
-
-        return new_video
-
-    @classmethod
-    async def get_videos_duration(cls, videos_ids: tuple[int]) -> int:
-        query = select(cls.duration).where(cls.id.in_(videos_ids))
-        durations: list[int] = await get_all(query)
-
-        return sum(durations)
-
-    async def delete(self) -> None:
-        current_complex = await Complex.get_by_id(self.complex_id)
-        current_complex.video_count -= 1
-        await current_complex.save()
-        await self.delete()
-
-
-class Avatar(BaseSQLModel, table=True):
+class Avatar(SQLModel, table=True):
     __tablename__ = 'avatars'
 
     id: int = Field(default=None, primary_key=True, index=True)
     file_name: str = Field(nullable=False, description="Имя файла аватарки")
 
-    @classmethod
-    async def get_first_id(cls):
-        query = select(cls.id).order_by(cls.id)
-        return await get_first(query)
 
-
-class Mood(BaseSQLModel, table=True):
+class Mood(SQLModel, table=True):
     __tablename__ = 'moods'
 
     id: int = Field(default=None, primary_key=True, index=True)
@@ -123,7 +65,7 @@ class Mood(BaseSQLModel, table=True):
                                 description="Название настроения (эмодзи)")
 
 
-class User(UserModel, table=True):
+class User(SQLModel, table=True):
     __tablename__ = 'users'
 
     id: int = Field(default=None, primary_key=True, index=True)
@@ -175,28 +117,8 @@ class User(UserModel, table=True):
     def __str__(self):
         return f"{self.email}"
 
-    async def level_up(self, next_complex_id: int) -> 'User':
-        if self.level < 10:
-            self.current_complex = next_complex_id
-            self.progress = 0
-            self.level += 1
-            await self.save()
-        return self
 
-    @classmethod
-    async def create(cls, data: dict) -> 'User':
-        data['password'] = await cls.get_hashed_password(data['password'])
-        data['avatar'] = await Avatar.get_first_id()
-        user = cls(**data)
-
-        return await user.save()
-
-    async def get_alarm_by_id(self, alarm_id: int) -> 'Alarm':
-        query = select(Alarm).where(Alarm.user_id == self.id).where(Alarm.id == alarm_id)
-        return await get_first(query)
-
-
-class Rate(BaseSQLModel, table=True):
+class Rate(SQLModel, table=True):
     __tablename__ = 'rates'
 
     id: int = Field(default=None, primary_key=True, index=True)
@@ -211,13 +133,8 @@ class Rate(BaseSQLModel, table=True):
     def __str__(self):
         return f"{self.id}: {self.name}"
 
-    @classmethod
-    async def get_free(cls) -> 'Rate':
-        query = select(cls).where(cls.price == 0)
-        return await get_first(query)
 
-
-class Administrator(AdminModel, table=True):
+class Administrator(SQLModel, table=True):
     __tablename__ = 'admins'
 
     id: int = Field(default=None, primary_key=True, index=True)
@@ -227,7 +144,7 @@ class Administrator(AdminModel, table=True):
     name: str = Field(nullable=True, default=None)
 
 
-class Alarm(BaseSQLModel, table=True):
+class Alarm(SQLModel, table=True):
     __tablename__ = 'alarms'
 
     id: int = Field(default=None, primary_key=True, index=True)
@@ -244,15 +161,8 @@ class Alarm(BaseSQLModel, table=True):
     def __str__(self):
         return f"{self.text}"
 
-    @classmethod
-    async def get_all_by_user_id(cls, user_id: int) -> list[BaseSQLModel]:
-        """Join cls rows with User table where User.id == user_id"""
 
-        query = select(cls).join(User).where(User.id == user_id).order_by(desc(cls.id))
-
-        return await get_all(query)
-
-class Notification(BaseSQLModel, table=True):
+class Notification(SQLModel, table=True):
     __tablename__ = 'notifications'
 
     id: int = Field(default=None, primary_key=True, index=True)
@@ -264,16 +174,8 @@ class Notification(BaseSQLModel, table=True):
     def __str__(self):
         return f"{self.text}"
 
-    @classmethod
-    async def get_all_by_user_id(cls, user_id: int) -> list[BaseSQLModel]:
-        """Join cls rows with User table where User.id == user_id"""
 
-        query = select(cls).join(User).where(User.id == user_id)
-
-        return await get_all(query)
-
-
-class ViewedComplex(BaseSQLModel, table=True):
+class ViewedComplex(SQLModel, table=True):
     __tablename__ = 'viewed_complexes'
 
     id: int = Field(default=None, primary_key=True, index=True)
@@ -288,65 +190,8 @@ class ViewedComplex(BaseSQLModel, table=True):
     complex_id: int = Field(nullable=False, foreign_key='complexes.id')
     complexes: 'Complex' = Relationship(back_populates="viewed_complexes")
 
-    @classmethod
-    async def add_viewed(
-            cls, user_id: int, complex_id: int
-    ) -> 'ViewedComplex':
 
-        query = select(cls).where(cls.user_id == user_id).where(cls.complex_id == complex_id)
-        complex_exists = await get_all(query)
-        if not complex_exists:
-            viewed_complex = cls(
-                user_id=user_id, complex_id=complex_id, viewed_at=get_current_datetime()
-            )
-            await viewed_complex.save()
-
-            return viewed_complex
-
-    @classmethod
-    async def get_all_viewed_complexes(
-            cls,
-            user_id: int
-    ) -> list['ViewedComplex']:
-
-        query = select(cls).where(cls.user_id == user_id)
-        return await get_all(query)
-
-    @classmethod
-    async def get_all_viewed_complexes_ids(
-            cls,
-            user_id: int
-    ) -> list[int]:
-
-        query = select(cls.id).where(cls.user_id == user_id)
-        return await get_all(query)
-
-    @classmethod
-    async def is_viewed_complex(
-            cls,
-            user_id: int,
-            complex_id: int
-    ) -> 'ViewedComplex':
-
-        query = select(cls).where(cls.user_id == user_id).where(cls.id == complex_id)
-        return await get_first(query)
-
-    @classmethod
-    async def is_last_viewed_today(cls, user_id: int) -> bool:
-        """
-        Check Complex viewed today
-
-        True if viewed  else False
-        """
-
-        current_day = get_current_datetime().day
-        query = select(cls).where(cls.user_id == user_id).order_by(cls.viewed_at)
-        last: ViewedComplex = await get_first(query)
-        if last and last.viewed_at:
-            return current_day == last.viewed_at.day
-
-
-class ViewedVideo(BaseSQLModel, table=True):
+class ViewedVideo(SQLModel, table=True):
     __tablename__ = 'viewed_videos'
 
     id: int = Field(default=None, primary_key=True, index=True)
@@ -357,26 +202,8 @@ class ViewedVideo(BaseSQLModel, table=True):
     video_id: int = Field(nullable=False, foreign_key='videos.id')
     videos: 'Video' = Relationship(back_populates="viewed_videos")
 
-    @classmethod
-    async def add_viewed(cls, user_id: int, video_id: int) -> 'ViewedVideo':
-        query = select(cls).where(cls.user_id == user_id).where(cls.video_id == video_id)
-        video_exists = await get_first(query)
-        if not video_exists:
-            viewed_video = cls(user_id=user_id, video_id=video_id)
-            await viewed_video.save()
 
-            return viewed_video
-
-    @classmethod
-    async def get_all_viewed_videos(
-            cls, user_id: int
-    ) -> list['ViewedVideo']:
-        query = select(cls).where(cls.user_id == user_id)
-
-        return await get_all(query)
-
-
-class Payment(BaseSQLModel, table=True):
+class Payment(SQLModel, table=True):
     __tablename__ = 'payments'
 
     id: int = Field(default=None, primary_key=True, index=True)
@@ -389,14 +216,8 @@ class Payment(BaseSQLModel, table=True):
     rate_id: int = Field(nullable=False, foreign_key='rates.id')
     rates: 'Rate' = Relationship(back_populates="payments")
 
-    @classmethod
-    async def get_by_user_and_rate_id(cls, user_id: int, rate_id: int) -> 'Payment':
-        query = select(cls).where(cls.user_id == user_id).where(cls.rate_id == rate_id)
-        result = await get_first(query)
-        return result
 
-
-class PaymentCheck(BaseSQLModel, table=True):
+class PaymentCheck(SQLModel, table=True):
     __tablename__ = 'payment_checks'
 
     id: int = Field(default=None, primary_key=True, index=True)
@@ -424,11 +245,3 @@ class PaymentCheck(BaseSQLModel, table=True):
     rate_id: int= Field(nullable=False, foreign_key='rates.id',
                         description="Named 'order_num' in Prodamus report")
     rates: 'Rate' = Relationship(back_populates="payment_checks")
-
-    @classmethod
-    async def get_all_by_user_id(cls, user_id: int) -> list['PaymentCheck']:
-        """Return all rows by user_id"""
-
-        query = select(cls).where(cls.user_id == user_id)
-        return await get_all(query)
-
