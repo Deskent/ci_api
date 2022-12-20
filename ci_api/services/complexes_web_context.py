da73e26c1c15d9e9e55d3dc9b575e8894b93d286
Deskent <battenetciz@gmail.com>
2022-12-20 14:49:03 +0300

    fix converting for complexes duration
    [build:1.11.10]
    [version:1.0]

--- ci_api/services/complexes_web_context.py
+++ ci_api/services/complexes_web_context.py
@@ -4,14 +4,14 @@ from models.models import User, Complex, ViewedComplex
 from schemas.complexes_videos import ComplexesListWithViewedAndNot
 from schemas.user_schema import UserOutput
 from services.models_cache.crud import CRUD
-from services.utils import convert_seconds_to_time
+from services.utils import convert_seconds_to_time, convert_to_minutes
 from services.web_context_class import WebContext
 
 def _convert_duration_from_int_to_time(data: Iterable) -> Iterable:
     """Convert complex duration from int to time format"""
 
     for elem in data:
-        elem.duration = convert_seconds_to_time(elem.duration)
+        elem.duration = convert_to_minutes(elem.duration)
 
     return data
 
@@ -53,7 +53,7 @@ async def get_complexes_list_web_context(
         today_complex: Complex = await CRUD.complex.get_by_id(user.current_complex)
         today_complex.duration = convert_seconds_to_time(today_complex.duration)
 
-    complexes: list[Complex] = await CRUD.complex.get_all()
+    complexes: list[Complex] = await CRUD.complex.get_all(use_cache=False)
     user_viewed_complexes: list[int] = await ViewedComplex.get_all_viewed_complexes_ids(user.id)
     viewed_complexes: list[Complex] = _get_viewed_complexes(complexes, user_viewed_complexes)
     not_viewed_complexes: list[Complex] = _get_not_viewed_complexes(complexes, user_viewed_complexes)
