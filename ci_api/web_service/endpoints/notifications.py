c161afb549f2906084128a8b42ea599597d4b82b
Deskent <battenetciz@gmail.com>
2022-12-20 21:34:44 +0300

    fix to CRUD
    [build:1.11.15]
    [version:1.0]

--- ci_api/web_service/endpoints/notifications.py
+++ ci_api/web_service/endpoints/notifications.py
@@ -2,6 +2,7 @@ from fastapi import Depends, APIRouter
 from starlette.responses import HTMLResponse, RedirectResponse
 
 from config import templates
+from crud_class.crud import CRUD
 from models.models import User, Notification
 from web_service.utils.title_context_func import get_page_titles
 from web_service.utils.get_contexts import get_user_browser_session, get_logged_user_context
@@ -13,7 +14,7 @@ router = APIRouter(tags=['web', 'notifications'])
 async def delete_notification(
         notification_id: int,
 ):
-    await Notification.delete_by_id(notification_id)
+    await CRUD.notification.delete_by_id(notification_id)
 
     return RedirectResponse("/complexes_list")
 
@@ -23,7 +24,7 @@ async def notifications_get(
         context: dict = Depends(get_logged_user_context),
         user: User = Depends(get_user_browser_session)
 ):
-    notifications: list = await Notification.get_all_by_user_id(user.id)
+    notifications: list = await CRUD.notification.get_all_by_user_id(user.id)
     context.update(notifications=notifications)
 
     return templates.TemplateResponse(
