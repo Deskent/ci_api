67c70f79f01a8ba0e0a6dc6e3d73c5c06b1892e5
Deskent <battenetciz@gmail.com>
2022-12-20 17:34:59 +0300

    ref CRUD
    [build:1.11.13]
    [version:1.0]

--- ci_api/services/videos_methods.py
+++ ci_api/services/videos_methods.py
@@ -1,6 +1,7 @@
 from exc.exceptions import UserNotFoundError, UserNotFoundErrorApi
 from models.models import User, Video, Complex, ViewedComplex
 from services.complexes_and_videos import is_video_viewed_before, check_level_up
+from services.models_cache.crud import CRUD
 from services.utils import convert_seconds_to_time, convert_to_minutes
 from services.web_context_class import WebContext
 
@@ -12,8 +13,8 @@ async def get_viewed_video_response(user: User, video_id: int, context: dict) ->
         web_context.to_raise = UserNotFoundErrorApi
         return web_context
 
-    current_video: Video = await Video.get_by_id(video_id)
-    next_video_id: int = await current_video.next_video_id()
+    current_video: Video = await CRUD.video.get_by_id(video_id)
+    next_video_id: int = await CRUD.video.next_video_id(current_video)
 
     if not next_video_id:
         web_context.template = "come_tomorrow.html"
@@ -21,7 +22,7 @@ async def get_viewed_video_response(user: User, video_id: int, context: dict) ->
 
         return web_context
 
-    videos: list[Video] = await Video.get_all_by_complex_id(user.current_complex)
+    videos: list[Video] = await CRUD.video.get_all_by_complex_id(user.current_complex)
     data = dict(next_video_id=next_video_id, videos=videos)
     web_context.context.update(**data)
     web_context.api_data.update(payload=data)
@@ -33,13 +34,13 @@ async def get_viewed_video_response(user: User, video_id: int, context: dict) ->
 
     old_user_level: int = user.level
     new_user: User = await check_level_up(user)
-    current_complex: Complex = await Complex.get_by_id(user.current_complex)
-    web_context.context.update(current_complex=current_complex)
     if new_user.level <= old_user_level:
+        current_complex: Complex = await Complex.get_by_id(user.current_complex)
+        web_context.context.update(current_complex=current_complex)
         web_context.redirect = f"/startCharging/{next_video_id}"
         return web_context
 
-    current_complex: Complex = await Complex.get_by_id(user.current_complex)
+    current_complex: Complex = await CRUD.complex.get_by_id(user.current_complex)
     web_context.context.update(user=new_user, current_complex=current_complex)
     web_context.template = "new_level.html"
 
@@ -51,10 +52,10 @@ async def get_viewed_complex_response(
         complex_id: int
 ) -> dict:
     result = {"level_up": False}
-    if not await ViewedComplex.is_viewed_complex(user_id=user.id, complex_id=complex_id):
-        await ViewedComplex.add_viewed(user_id=user.id, complex_id=complex_id)
-        next_complex: Complex = await Complex.get_next_complex(user.current_complex)
-        user: User = await user.level_up(next_complex.id)
+    if not await CRUD.viewed_complex.is_viewed_complex(user_id=user.id, complex_id=complex_id):
+        await CRUD.viewed_complex.add_viewed(user_id=user.id, complex_id=complex_id)
+        next_complex: Complex = await CRUD.complex.get_next_complex(user.current_complex)
+        user: User = await CRUD.user.level_up(user)
         next_complex_duration: int = convert_to_minutes(next_complex.duration)
         result.update(
             {
