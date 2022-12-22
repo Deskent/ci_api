886f5c98cd36e09bc6fbd13cd4c971a3a041b756
Deskent <battenetciz@gmail.com>
2022-12-22 15:54:44 +0300

    add modals
    [build:1.11.22]
    [version:1.0]

--- static/js/profile_popup.js
+++ static/js/profile_popup.js
@@ -16,101 +16,103 @@ const formEmojiImg = modal?.querySelectorAll('.form_emoji__img');
 
 const formEmojiInput = modal?.querySelectorAll('.form_emoji-input');
 
-
-
+const newLevelHeader = document.querySelector('.new-level_header');
 
 
 if (urlPath.includes("profile")) {
-  document.addEventListener("DOMContentLoaded", async (evt) => {
-      const response = await fetch(serverUrl + '/web/check_first_entry', {
-          method: 'GET',
-          headers: {
-              'Content-Type': 'text/html;charset=utf-8',
-          },
-      });
-
-      let result = await response.json();
-      console.log(result);
-
-      const newUser = result.new_user;
-      const todayFirstEntry = result.today_first_entry;
-      const isExpired = result.is_expired;
-      const emojies = result.emojies;
-
-
-      if(newUser === true) {
-        formEmojiBox.remove();
-        openModalNewLevelHandler();
-        helloVideoPlay?.addEventListener("click", (evt) => {
-          helloVideoPlay?.classList.add("hidden");
-          helloVideoPause?.classList.remove("hidden");
-          helloVideo.play();
-        })
-
-        helloVideoPause?.addEventListener("click", (evt) => {
-          helloVideoPlay?.classList.remove("hidden");
-          helloVideoPause?.classList.add("hidden");
-          helloVideo.pause();
-        })
-      }
-
-      if(todayFirstEntry === true) {
-        openModalNewLevelHandler();
-
-        console.log(emojies);
-
-        helloVideoWrapper.remove();
-        newLevelDescription.remove();
-        const newLevelHeader = document.querySelector('.new-level_header');
-
-        newLevelHeader.textContent = "Какое у Вас сегодня настроение?"
-        
-        for (let i = 0; i < formEmojiBoxInput.length; i++) {
-          // const formEmojiBoxInput = formEmojiBoxInputArr[i];
-          const formEmojiLabelItem = formEmojiLabel[i];
-          const formEmojiInputItem = formEmojiInput[i]
-
-          formEmojiLabelItem.textContent = emojies[i].name;
-          formEmojiInputItem.setAttribute('id', emojies[i].id);
-          formEmojiInputItem.setAttribute('value', emojies[i].name);
-          formEmojiLabelItem.setAttribute('for', emojies[i].id);
-        }
-
-        formEmoji.addEventListener('submit', async(evt) => {
-          evt.preventDefault();
-
-          let inputId;
-          for (let input of formEmojiInput) {
-            if(input.checked) {
-              inputId = Number(input.id);
-            }
-          };
-          console.log(inputId);
-
-          const request = fetch(serverUrl + '/web/set_user_mood', {
-            method: 'POST',
-            body: JSON.stringify({
-              'mood_id': inputId,
-            }),
+    document.addEventListener("DOMContentLoaded", async (evt) => {
+        const response = await fetch(serverUrl + '/web/check_first_entry', {
+            method: 'GET',
             headers: {
-              'Content-Type': 'application/json;charset=utf-8',
-            }
+                'Content-Type': 'text/html;charset=utf-8',
+            },
         });
 
-        let answer = await request.json;
+        let result = await response.json();
+        console.log(result);
 
-        console.log(answer);
-        })
-        
-      }
+        const newUser = result.new_user;
+        const todayFirstEntry = result.today_first_entry;
+        const isExpired = result.is_expired;
+        const emojies = result.emojies;
 
-      if(isExpired === true) {
-        helloVideoWrapper.remove();
-        formEmojiBox.remove();
-        newLevelHeader.textContent = "Подписка закончилась"
-      }
 
-      close?.addEventListener("click", closeModalNewLevelHandler);
+        if (newUser === true) {
+            formEmojiBox.remove();
+            openModalNewLevelHandler();
+            helloVideoPlay?.addEventListener("click", (evt) => {
+                helloVideoPlay?.classList.add("hidden");
+                helloVideoPause?.classList.remove("hidden");
+                helloVideo.play();
+            })
 
-  })
+            helloVideoPause?.addEventListener("click", (evt) => {
+                helloVideoPlay?.classList.remove("hidden");
+                helloVideoPause?.classList.add("hidden");
+                helloVideo.pause();
+            })
+        }
+
+        if (todayFirstEntry === true) {
+            openModalNewLevelHandler();
+
+            console.log(emojies);
+
+            helloVideoWrapper.remove();
+            newLevelDescription.remove();
+
+
+            newLevelHeader.textContent = "Какое у Вас сегодня настроение?"
+
+            for (let i = 0; i < formEmojiBoxInput.length; i++) {
+                // const formEmojiBoxInput = formEmojiBoxInputArr[i];
+                const formEmojiLabelItem = formEmojiLabel[i];
+                const formEmojiInputItem = formEmojiInput[i]
+
+                formEmojiLabelItem.textContent = emojies[i].name;
+                formEmojiInputItem.setAttribute('id', emojies[i].id);
+                formEmojiInputItem.setAttribute('value', emojies[i].name);
+                formEmojiLabelItem.setAttribute('for', emojies[i].id);
+            }
+
+            formEmoji.addEventListener('submit', async (evt) => {
+                evt.preventDefault();
+
+                let inputId;
+                for (let input of formEmojiInput) {
+                    if (input.checked) {
+                        inputId = Number(input.id);
+                    }
+                }
+                ;
+                console.log(inputId);
+
+                const request = fetch(serverUrl + '/web/set_user_mood', {
+                    method: 'POST',
+                    body: JSON.stringify({
+                        'mood_id': inputId,
+                    }),
+                    headers: {
+                        'Content-Type': 'application/json;charset=utf-8',
+                    }
+                });
+
+                let answer = await request.json;
+
+                console.log(answer);
+            })
+
+        }
+
+        if (isExpired === true) {
+            openModalNewLevelHandler();
+            newLevelDescription.remove();
+            helloVideoWrapper.remove();
+            formEmojiBox.remove();
+            newLevelHeader.textContent = "Подписка закончилась"
+        }
+
+        close?.addEventListener("click", closeModalNewLevelHandler);
+
+    })
 }
