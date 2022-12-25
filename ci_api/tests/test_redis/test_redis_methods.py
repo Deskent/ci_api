a5143bd347373a75262ff90de6f4eb42ac47be32
Deskent <battenetciz@gmail.com>
2022-12-25 17:10:07 +0300

    add tests
    [build:1.11.25]
    [version:1.0]

--- ci_api/tests/test_redis/test_redis_methods.py
+++ ci_api/tests/test_redis/test_redis_methods.py
@@ -1,51 +1,47 @@
 import pytest
 
-from config import REDIS_CLIENT
-from crud_class.crud import CRUD
+from config import get_redis_client
 from misc.redis_class import RedisDB
-from database.models import User, Rate
+from database.models import Rate
 
 
 @pytest.fixture
-def redis():
-    return RedisDB(client=REDIS_CLIENT, model=User)
+def redis_client():
+    return get_redis_client()
 
 
-@pytest.mark.server
-async def test_redis_health_check(redis):
+@pytest.mark.manual
+async def test_redis_health_check(event_loop, redis_client):
 
-    result = await redis._health_check()
+    result = await RedisDB(model=Rate, client=redis_client)._health_check()
     assert result == {'test': 'test'}
-    assert await redis.delete_key() is None
+    assert await RedisDB(model=Rate, client=get_redis_client()).delete_key() is None
 
 
-@pytest.mark.server
-async def test_redis_save_load_rates():
-    redis = RedisDB(model=Rate, client=REDIS_CLIENT)
-    rates: list[Rate] = await CRUD.rate.get_all()
-    await redis.save_all(rates, 60)
+@pytest.mark.skip
+async def test_redis_save_load_rates(all_rates, event_loop, redis_client):
+    redis = RedisDB(model=Rate, client=redis_client)
+    await redis.save_all(all_rates, 60)
     result: list[Rate] = await redis.load_all()
-    assert rates == result
+    assert all_rates[:] == result
     assert await redis.delete_key() is None
 
 
-@pytest.mark.server
-async def test_redis_get_by_id():
+@pytest.mark.skip
+async def test_redis_get_by_id(all_rates, event_loop, redis_client):
     rate_id = 1
-    redis = RedisDB(model=Rate, client=REDIS_CLIENT)
-    rates: list[dict] = await CRUD.rate.get_all()
-    await redis.save_all(rates, 60)
+    redis = RedisDB(model=Rate, client=redis_client)
+    await redis.save_all(all_rates, 60)
     result = await redis.get_by_id(rate_id)
     assert result.id == rate_id
 
 
-@pytest.mark.server
-async def test_redis_save_by_id():
-    redis = RedisDB(model=Rate, client=REDIS_CLIENT)
-    rates: list[dict] = await CRUD.rate.get_all()
-    await redis.save_all(rates, 60)
+@pytest.mark.skip
+async def test_redis_save_by_id(all_rates, event_loop, redis_client):
+    redis = RedisDB(model=Rate, client=redis_client)
+    await redis.save_all(all_rates, 60)
     new_rate = Rate(**{
-                'id': 3,
+                'id': 10000,
                 'name': 'New_test_rate',
                 'price': -15,
                 'duration': 1
