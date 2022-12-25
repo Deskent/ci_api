0b284e7cdd24c40ac02f1e0b5f2b29bcbfe89417
Deskent <battenetciz@gmail.com>
2022-12-25 17:09:42 +0300

    fix test_get_video_by_id
    [build:1.11.25]
    [version:1.0]

--- ci_api/tests/test_endpoints/test_authorized_endpoints/test_video_endpoints.py
+++ ci_api/tests/test_endpoints/test_authorized_endpoints/test_video_endpoints.py
@@ -1,13 +1,12 @@
 import pytest
 
-from crud_class.crud import CRUD
 from tests.test_endpoints.test_authorized_endpoints.base_test_class import BaseTest
 
 
 class TestVideo(BaseTest):
 
     @pytest.mark.server
-    def test_all_videos_for_complex_id(self):
+    def test_all_videos_for_complex_id(self, event_loop):
         response = self.session.get(self.base_url + "/videos/all_for/1", headers=self.headers)
         assert response.status_code == 200
         data: list = response.json()
@@ -18,7 +17,9 @@ class TestVideo(BaseTest):
     def test_get_video_by_id(self, event_loop):
         response = self.session.get(self.base_url + f"/complex/1/", headers=self.headers)
         assert response.status_code == 200
-        complexes: dict = response.json()
-        video_id: int = complexes['videos'][0].get('id')
+        complex_: dict = response.json()
+        videos: list[dict] = complex_['videos']
+        assert videos is not None
+        video_id: int = videos[0].get('id')
         response = self.session.get(self.base_url + f"/videos/{video_id}/", headers=self.headers)
         assert response.status_code == 200
