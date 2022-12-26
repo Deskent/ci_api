c96cb0bb96339e2cd76b699148a6096ca22961c3
Deskent <battenetciz@gmail.com>
2022-12-26 16:36:05 +0300

    fix set_viewed route: now POST not GET
    [build:1.11.26]
    [version:1.0]

--- ci_api/api/complexes.py
+++ ci_api/api/complexes.py
@@ -4,7 +4,7 @@ from crud_class.crud import CRUD
 from database.models import User, Complex, Video
 from exc.exceptions import ComplexNotFoundError
 from misc.web_context_class import WebContext
-from schemas.complexes_videos import ComplexData, ComplexesListWithViewedAndNot
+from schemas.complexes_videos import ComplexData, ComplexesListWithViewedAndNot, ComplexID
 from services.complexes_web_context import get_complexes_list_web_context, \
     get_all_complexes_web_context
 from services.depends import get_logged_user
@@ -34,18 +34,6 @@ async def get_complexes_list_api(
 
 
 @router.get(
-    "/all",
-    response_model=list[Complex],
-    dependencies=[Depends(get_logged_user)]
-)
-async def get_all_complexes_api():
-    """Return all complexes as JSON"""
-
-    web_context: WebContext = await get_all_complexes_web_context({})
-    return web_context.api_render()
-
-
-@router.get(
     "/{complex_id}",
     response_model=ComplexData,
     dependencies=[Depends(get_logged_user)]
@@ -62,13 +50,13 @@ async def complex_data(
     return ComplexData(**complex_.dict(), videos=videos)
 
 
-@router.get(
-    "/complex_viewed/{complex_id}",
+@router.post(
+    "/set_viewed",
     status_code=status.HTTP_200_OK,
     response_model=dict
 )
 async def complex_viewed_api(
-        complex_id: int,
+        complex: ComplexID,
         user: User = Depends(get_logged_user)
 ):
     """
@@ -80,4 +68,4 @@ async def complex_viewed_api(
     {"level_up": False, "new_level": user.level, "next_complex_duration": next_complex_duration}
     """
 
-    return await get_viewed_complex_response(user=user, complex_id=complex_id)
+    return await get_viewed_complex_response(user=user, complex_id=complex.id)
