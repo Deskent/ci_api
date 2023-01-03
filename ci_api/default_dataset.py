14fae1beae3f1c4e7b8760c860e79d73502e5e6f
Deskent <battenetciz@gmail.com>
2023-01-03 14:04:57 +0300

    create
    [build:1.12.3]
    [version:1.0]

---
+++ ci_api/default_dataset.py
@@ -0,0 +1,103 @@
+complex_data = [
+    {
+        "description": "Описание комплекса 1",
+        "name": "комплекс 1",
+        "number": 1,
+        "duration": 0
+    },
+]
+
+videos_data = [
+    {
+        "complex_id": 1,
+        'file_name': 'e1c1.mp4',
+        'description': f'описание видео {1}',
+        'name': f'Видео № {1}',
+        'duration': 75,
+        'number': 1
+    },
+    {
+        "complex_id": 1,
+        'file_name': 'e2c1.mp4',
+        'description': f'описание видео {2}',
+        'name': f'Видео № {2}',
+        'duration': 66,
+        'number': 2
+    },
+    {
+        "complex_id": 1,
+        'file_name': 'e3c1.mp4',
+        'description': f'описание видео {3}',
+        'name': f'Видео № {3}',
+        'duration': 90,
+        'number': 3
+    },
+    {
+        "complex_id": 1,
+        'file_name': 'e4c1.mp4',
+        'description': f'описание видео {4}',
+        'name': f'Видео № {4}',
+        'duration': 75,
+        'number': 4
+    },
+    {
+        "complex_id": 1,
+        'file_name': 'e5c1.mp4',
+        'description': f'описание видео {5}',
+        'name': f'Видео № {5}',
+        'duration': 66,
+        'number': 5
+    },
+]
+
+hello_video = [
+    {
+        'file_name': 'hello.mp4',
+        'description': 'Приветственное видео',
+        'name': 'Приветственное видео',
+        'duration': 30,
+        'number': 1
+    },
+]
+
+avatar = [
+    {
+        'file_name': 'avatar.svg',
+    },
+]
+
+moods = [
+            {
+                'name': 'Все бесят',
+                'code': 'U+1F621'
+            },
+            {
+                'name': 'Печалька-тоска',
+                'code': 'U+1F61E'
+            },
+            {
+                'name': 'Нервно-тревожно',
+                'code': 'U+1F910'
+            },
+            {
+                'name': 'Бодрячок',
+                'code': 'U+1F601'
+            },
+            {
+                'name': 'Всех люблю',
+                'code': 'U+1F970'
+            },
+        ]
+
+rates = [
+            {
+                'name': 'Free',
+                'price': 0,
+                'duration': 30
+            },
+            {
+                'name': 'Солнце',
+                'price': 199,
+                'duration': 30
+            },
+        ]
