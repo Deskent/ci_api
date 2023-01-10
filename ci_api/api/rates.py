af7b857fbb1e8d48ad1925a603d6d9858371d3b3
Deskent <battenetciz@gmail.com>
2023-01-10 11:26:03 +0300

    add /rates/{rate_id} add RateLink schema

--- ci_api/api/rates.py
+++ ci_api/api/rates.py
@@ -2,7 +2,10 @@ from fastapi import APIRouter, Depends, status
 
 from crud_class.crud import CRUD
 from database.models import User, Rate
+from misc.web_context_class import WebContext
+from schemas.rates import RateLink
 from services.depends import get_logged_user
+from web_service.utils.payments_context import get_subscribe_by_rate_id
 
 router = APIRouter(prefix="/rates", tags=['Rates'])
 
@@ -33,3 +36,23 @@ async def get_user_rate(
     """
 
     return await CRUD.rate.get_by_id(user.rate_id)
+
+
+@router.get(
+    "/{rate_id}",
+    response_model=RateLink,
+    status_code=status.HTTP_200_OK
+)
+async def get_rate_by_id_api(
+        rate_id: int,
+        user: User = Depends(get_logged_user),
+):
+    """
+    Return link for pay subscribe. Need authorization.
+
+    :return: JSON with link
+    """
+
+    context = dict(user=user)
+    web_context: WebContext = await get_subscribe_by_rate_id(context, rate_id)
+    return web_context.api_render()
