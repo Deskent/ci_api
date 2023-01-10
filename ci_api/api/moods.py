6054f0219bd91cfa805be4d95a9db2f0f002b05b
Deskent <battenetciz@gmail.com>
2023-01-10 14:07:41 +0300

    move set_user_mood to moods route

--- ci_api/api/moods.py
+++ ci_api/api/moods.py
@@ -3,6 +3,7 @@ from starlette import status
 
 from crud_class.crud import CRUD
 from database.models import User, Mood
+from schemas.user_schema import UserMood
 from services.depends import get_logged_user
 
 router = APIRouter(prefix="/moods", tags=['Moods'])
@@ -28,7 +29,7 @@ async def get_all_moods():
     response_model=Mood,
     status_code=status.HTTP_200_OK,
 )
-async def get_user_avatar(
+async def get_user_mood(
         user: User = Depends(get_logged_user),
 ):
     """Return user mood info. Need authorization.
@@ -36,4 +37,22 @@ async def get_user_avatar(
     :return: Mood as JSON
     """
 
-    return await CRUD.mood.get_by_id(user.avatar)
+    return await CRUD.mood.get_by_id(user.mood)
+
+
+@router.put(
+    "/set_user_mood",
+    status_code=status.HTTP_204_NO_CONTENT,
+    response_model=None
+)
+async def set_user_mood(
+        mood: UserMood,
+        user: User = Depends(get_logged_user)
+):
+    """
+    Set mood for user. Need authorization.
+
+    :return: null
+    """
+
+    await CRUD.user.set_mood(mood.mood_id, user=user)
